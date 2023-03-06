f314345d35e16a59d947bb4e20e56870111473b1
Davod3 <davidpereira.pereira4@gmail.com>
2023-03-06 16:00:36 +0000

    Started server logic

--- src/Server/TintoIMarketServer.java
+++ src/Server/TintoIMarketServer.java
@@ -3,7 +3,19 @@ package Server;
 public class TintoIMarketServer {
 
 	public static void main(String[] args) {
-		System.out.println("123");
+		
+		int port = 1234;
+		
+		if(args.length >= 1){
+			
+			//Set port
+			port = Integer.parseInt(args[0]);
+			
+		}
+		
+		//Create Server
+		Server myServer = new Server(port);
+		myServer.run();
 
 	}
 }
