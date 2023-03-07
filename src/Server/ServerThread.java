6e6c10f95920bfde23757c5fe8d6f71c13378608
Davod3 <davidpereira.pereira4@gmail.com>
2023-03-07 15:30:30 +0000

    Added receive file function on server

--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -1,5 +1,7 @@
 package Server;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -9,49 +11,50 @@ import Catalogs.UserCatalog;
 import domain.User;
 
 public class ServerThread extends Thread {
-	
+
 	private Socket socket = null;
 	private UserCatalog userCatalog;
 	private User loggedUser = null;
-	
+	private ObjectOutputStream outStream;
+	private ObjectInputStream inStream;
+
 	public ServerThread(Socket inSoc) throws IOException {
-		System.out.println("Opened clietn socket");
 		this.socket = inSoc;
-		System.out.println("New connection established!");
-		
 		this.userCatalog = UserCatalog.getInstance();
+		this.outStream = new ObjectOutputStream(socket.getOutputStream());
+		this.inStream = new ObjectInputStream(socket.getInputStream());
+
+		System.out.println("New connection established!");
 	}
-	
+
 	public void run() {
-		
-		//Open IO streams
+
+		// Open IO streams
 		try {
-			
-			ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
-			ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
-		
-			
+
 			String user = null;
 			String pwd = null;
-			
+
 			System.out.println(inStream.available());
-			
+
 			user = (String) inStream.readObject();
 			pwd = (String) inStream.readObject();
-			
+
 			boolean value = false;
-			
-			if(value = (this.loggedUser = userCatalog.validate(user,pwd)) != null) {
-				//User authenticated, wait for commands
+
+			if (value = (this.loggedUser = userCatalog.validate(user, pwd)) != null) {
+				// User authenticated, wait for commands
 				System.out.println("User authenticated");
 				outStream.writeObject(value);
+
+				mainLoop();
+
 			} else {
-				//User failed to authenticate, close connection
+				// User failed to authenticate, close connection
 				System.out.println("Authentication failed");
 				outStream.writeObject(value);
 			}
-		
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -59,8 +62,81 @@ public class ServerThread extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+	}
+
+	private void mainLoop() throws ClassNotFoundException, IOException {
+		// Run main command execution logic
+
+		while (this.socket.isConnected()) {
+
+			System.out.println("Waiting for commands...");
+
+			String command = (String) inStream.readObject();
+			
+			switch (command) {
+			
+			case "add":
+				
+				String wine = (String) inStream.readObject();
+				File received = receiveFile();
+				
+				break;
+			
+			case "sell":
+				
+				break;
+
+			case "view":
+				
+				break;
+			
+			case "buy":
+				
+				break;
+			
+			case "wallet":
+				
+				break;
+			
+			case "classify":
+				
+				break;
+				
+			case "talk":
+				
+				break;
+			
+			case "read":
+				
+				break;
+				
+			default:
+				
+				//Default case for wrong command message
+				
+				break;
+			}
+
+		}
+
+	}
+
+	private File receiveFile() throws ClassNotFoundException, IOException {
 		
+		String name = (String) inStream.readObject();
+		int size = (Integer) inStream.readObject();
 		
+		byte[] bytes = new byte[size];
+		
+		inStream.read(bytes, 0, size);
+		
+		File outFile = new File("Recebido.txt");
+		
+		FileOutputStream fout = new FileOutputStream(outFile);
+		fout.write(bytes);
+		
+		return outFile;
 	}
 
 }
